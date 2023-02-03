4d221a9c8119ff12ddf88061c9c0fffeda4c33b5
Mingzhe Sun <sunmingzhe99@outlook.com>
2023-02-03 02:00:19 -0500

    test git acc

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ than one copy is in the rectangle (the
 texture_wrap).
 
 '''
-#commit test
+#commit test1
 
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, ListProperty, StringProperty
